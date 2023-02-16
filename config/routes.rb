027b0c9ea50e52e28d1baa0c64d024bd39b67d74
iSaacSigei <kiplangatisack.is@gmail.com>
2023-02-16 11:35:32 +0300

    created API end points

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :notes
+  resources :users, only: [:show]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #signup
+  post '/signup', to: 'users#user_signup'
+
+  #login
+  post '/login', to: 'sessions#login'
+
+  #logout
+  delete '/logout', to: 'sessions#logout'
+
 end
