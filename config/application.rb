4e918bc054c71d825c82ef8b80e815a8db3d3e07
iSaacSigei <kiplangatisack.is@gmail.com>
2023-02-16 11:35:02 +0300

    added configurations to allow use of cookies and sessions

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Backend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
