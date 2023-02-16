a7e0f6440ac5d91c8290f9515e7db831367bbbde
iSaacSigei <kiplangatisack.is@gmail.com>
2023-02-16 11:32:13 +0300

    added login and logout methods using cookies and sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+     
+    def login
+        user=User.find_by(username:params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id]=user.id
+            render json: user, status: :created
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized   
+        end
+    end
+    def logout
+        session.delete(:user_id)
+        render json: { message: "Successfully logged out user" }, status: :ok
+      end
+end
