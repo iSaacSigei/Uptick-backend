b4d4b36749e69c97ddd6c1fd64a7eb10214d6852
iSaacSigei <kiplangatisack.is@gmail.com>
2023-02-16 11:31:03 +0300

    added helper methods to find the current logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    include AbstractController::Helpers
+
+    helper_method :current_user
+  
+    private
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
 end
